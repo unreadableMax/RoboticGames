e36b90452bf54bf562adcc100fc178b25da866af
alex <vx228@uni-heidelberg.de>
2019-11-27 17:25:59 +0100

    removed devel and build

--- build/exercise_1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/max/RoboticGames/catkin_ws/src/exercise_1
-# Build directory: /home/max/RoboticGames/catkin_ws/build/exercise_1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
